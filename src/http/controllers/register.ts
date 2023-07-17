43559409067ae14849c2e9b7d16f96e6247beaed
Jeremias Santos <jeremias.santos@newcheck.com.br>
2023-07-17 12:22:11 -0300

    feat: repository patterns

---
+++ src/http/controllers/register.ts
@@ -0,0 +1,21 @@
+import { z } from 'zod'
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { registerUseCase } from '@/use-cases/register'
+
+export async function register(req: FastifyRequest, res: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerBodySchema.parse(req.body)
+
+  try {
+    await registerUseCase({ name, email, password })
+  } catch (err) {
+    return res.status(409).send()
+  }
+
+  return res.status(201).send()
+}
