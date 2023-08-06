15bf6973f2f5d219eaa638f6f3a66c1e03602afe
Jeremias Santos <jeremias.santos@newcheck.com.br>
2023-08-05 23:08:33 -0300

    feat: metrics

---
+++ src/use-cases/get-user-metrics.spec.ts
@@ -0,0 +1,26 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+import { InMemoryCheckInsRepository } from '@/repositories/in-memcory/in-memory-check-ins-repository'
+import { GetUserMetricsUseCase } from './get-user-metrics'
+
+describe('Get user metrics use case', () => {
+  let checkInsRepository: InMemoryCheckInsRepository
+  let sut: GetUserMetricsUseCase
+
+  beforeEach(() => {
+    checkInsRepository = new InMemoryCheckInsRepository()
+    sut = new GetUserMetricsUseCase(checkInsRepository)
+  })
+
+  it('should be able to get check-ins count from metrics', async () => {
+    await checkInsRepository.create({
+      gym_id: 'gym-01',
+      user_id: 'user-01',
+    })
+    await checkInsRepository.create({
+      gym_id: 'gym-02',
+      user_id: 'user-01',
+    })
+    const { checkInsCount } = await sut.execute({ userId: 'user-01' })
+    expect(checkInsCount).toEqual(2)
+  })
+})
